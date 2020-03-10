49fe4c13500bbd3a51acebd382fa4b9b536c5b69
pavankpdev <pavankumarkpkp@gmail.com>
2020-03-10 17:18:35 +0530

    tmdb promise related bugs fixed

--- routes/api/Home.js
+++ routes/api/Home.js
@@ -11,16 +11,27 @@ const {
 } = require("../../config/keys");
 
 const API_QUERY = `api_key=${TMDB_API_KEY}&language=${TMDB_LANGUAGE}&page=1&region=${TMDB_REGION}`;
+let nowPlayingMoviesData = {};
+let upcomingMoviesData = {};
 
 // @Route   GET /
 // @des     EXTERNAL_API_CALL gets movies information as a json format
 // @access  PUBLIC
 router.get("/", async (req, res) => {
+  // function to load data to variable which was returned from promise
+  const loadDAtaToVariable = (nowPlayingMovies, upComingMovies) => {
+    nowPlayingMoviesData = { ...nowPlayingMovies };
+    upcomingMoviesData = { ...upComingMovies };
+  };
+
   // function to fetch all NOW_PLAYING movies from TMDB API
   const nowPlayingMovies = () => {
     fetch(`${TMDB_FETCH_MOVIES_BASE_URI}${TMDB_NOW_PLAYING_URI}${API_QUERY}`)
       .then(response => response.json())
-      .then(json => res.json(json))
+      .then(json => {
+        loadDAtaToVariable(json);
+        res.json(json);
+      })
       .catch(err => {
         if (err.name === "AbortError") {
           res.json({ error: err.name });
@@ -41,6 +52,7 @@ router.get("/", async (req, res) => {
         }
       });
   };
+  nowPlayingMovies();
 });
 
 module.exports = router;
