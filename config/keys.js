b414d3b316dcf54bec5f1af3d2decdac7f2dccc6
pavankpdev <pavankumarkpkp@gmail.com>
2020-03-10 09:54:45 +0530

    Implemented TMDB API for home page and node-fetch library to fetch now_playing and upcoming movies

--- config/keys.js
+++ config/keys.js
@@ -2,5 +2,13 @@ module.exports = {
   DB_CONNECTION_STRING:
     "mongodb+srv://pavan:pavan123@devconnector-r5es8.mongodb.net/movietown?retryWrites=true&w=majority",
   BCRYPT_SALT: 10,
-  JWT_PRIVATE_KEY: "MOVIE_TOWN_JWT_PRIVATE_KEY"
+  JWT_PRIVATE_KEY: "MOVIE_TOWN_JWT_PRIVATE_KEY",
+  TMDB_API_KEY: "a248d933920cd6d6527467220f28a930",
+  TMDB_LANGUAGE: "en-US",
+  TMDB_REGION: "US",
+  TMDB_NOW_PLAYING_URI: "now_playing?",
+  TMDB_UPCOMING_MOVIES_URI: "upcoming?",
+  TMDB_FETCH_MOVIES_BASE_URI: "https://api.themoviedb.org/3/movie/",
+  TMDB_BASE_URL: "http://image.tmdb.org/t/p/",
+  TMDB_POSTER_WIDTH: "w200"
 };
--- routes/api/Home.js
+++ routes/api/Home.js
@@ -1,5 +1,46 @@
+const fetch = require("node-fetch");
 const router = require("express").Router();
 
-router.get("/", (req, res) => res.json({ Message: "Hello KP!" }));
+const {
+  TMDB_FETCH_MOVIES_BASE_URI,
+  TMDB_UPCOMING_MOVIES_URI,
+  TMDB_NOW_PLAYING_URI,
+  TMDB_API_KEY,
+  TMDB_REGION,
+  TMDB_LANGUAGE
+} = require("../../config/keys");
+
+const API_QUERY = `api_key=${TMDB_API_KEY}&language=${TMDB_LANGUAGE}&page=1&region=${TMDB_REGION}`;
+
+// @Route   GET /
+// @des     EXTERNAL_API_CALL gets movies information as a json format
+// @access  PUBLIC
+router.get("/", async (req, res) => {
+  // function to fetch all NOW_PLAYING movies from TMDB API
+  const nowPlayingMovies = () => {
+    fetch(`${TMDB_FETCH_MOVIES_BASE_URI}${TMDB_NOW_PLAYING_URI}${API_QUERY}`)
+      .then(response => response.json())
+      .then(json => res.json(json))
+      .catch(err => {
+        if (err.name === "AbortError") {
+          res.json({ error: err.name });
+        }
+      });
+  };
+
+  // function to fetch all UPCOMING movies from TMDB API
+  const upComingMovies = () => {
+    fetch(
+      `${TMDB_FETCH_MOVIES_BASE_URI}${TMDB_UPCOMING_MOVIES_URI}${API_QUERY}`
+    )
+      .then(response => response.json())
+      .then(json => res.json(json))
+      .catch(err => {
+        if (err.name === "AbortError") {
+          res.json({ error: err.name });
+        }
+      });
+  };
+});
 
 module.exports = router;
