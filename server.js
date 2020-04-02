bb721dc9fe13ca4c0696abdcd53dba6f9f31270e
pavankpdev <pavankumarkpkp@gmail.com>
2020-04-02 20:32:20 +0530

    Initial Deployment configs

--- server.js
+++ server.js
@@ -34,15 +34,18 @@ app.use(passport.initialize());
 // Passport Config
 require("./config/passport")(passport);
 
-
 // serve static asset in production
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
+
+app.get("*", (req, res) => {
+  res.send("404 error invalid route");
+});
 
 const port = process.env.PORT || 4000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
